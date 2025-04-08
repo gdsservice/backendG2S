a82fbe89880a81eb08046b4664dff56d4fa1928c
sogoredev <sogoredev@gmail.com>
2025-04-08 09:17:26 +0000

    update

--- src/main/java/com/gds/Gestion/de/stock/security/SecurityController.java
+++ src/main/java/com/gds/Gestion/de/stock/security/SecurityController.java
@@ -25,11 +25,11 @@ public class SecurityController {
 
     @PostMapping("/login")
     private Map<String, String> conexion(@RequestBody AuthentificationDTO authentificationDTO){
+        System.out.println("TTTTTTTTTTTTTTTTTTT");
         Authentication authenticate = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authentificationDTO.username(), authentificationDTO.password())
         );
         if (authenticate.isAuthenticated()){
-            System.out.println("TTTTTTTTTTTTTTTTTTT");
             log.info("Resulta{}",authenticate.isAuthenticated());
             return jwtService.genererToken(authentificationDTO.username());
         }
