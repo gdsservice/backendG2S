2704ee2b22c5f4e33e40e575277a976110065ea6
sogoredev <sogoredev@gmail.com>
2025-05-03 20:35:41 +0000

    update image

--- src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
+++ src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins("https://stock.g2sservice.com")
+//                    fjsbk
 //                    .allowedOrigins("http://localhost:42817")
 //                .allowedOrigins("http://localhost:4200")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
