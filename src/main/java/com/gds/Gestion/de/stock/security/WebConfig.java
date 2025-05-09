7153f7c06a2ea22ed6d05bc8aedcef6a48d3a6d0
sogoredev <sogoredev@gmail.com>
2025-05-09 11:35:21 +0000

    update user role

--- src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
+++ src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-//                    .allowedOrigins("https://stock.g2sservice.com")
+                    .allowedOrigins("https://stock.g2sservice.com")
 //                    .allowedOrigins("http://localhost:42817")
-                .allowedOrigins("http://localhost:4200")
+//                .allowedOrigins("http://localhost:4200")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
