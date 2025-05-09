18554f4f7f0b72425d1cfd7a5edc2ab1dc207fa1
sogoredev <sogoredev@gmail.com>
2025-05-09 10:41:15 +0000

    update user roles

---
+++ src/main/resources/db/migration/V2__Initial_schema.sql
@@ -0,0 +1,3 @@
+UPDATE user_role SET name = 'ADMIN' WHERE name = 'ROLE_ADMIN';
+UPDATE user_role SET name = 'USER' WHERE name = 'ROLE_USER';
+UPDATE user_role SET name = 'SUPER_ADMIN' WHERE name = 'ROLE_SUPER_ADMIN';
